ff1ab036c4318ab16e535b728fe9debd6df6253c
jan <tech@kampherbeek.com>
2022-01-20 15:06:11 +0100

    Fix for unit test TestRosetta. Added more testmethods.

--- E4CTest/be/TestRosetta.cs
+++ E4CTest/be/TestRosetta.cs
@@ -9,23 +9,68 @@ using System.Text;
 using System.Threading.Tasks;
 
 using E4C.be.domain;
+using E4C.be.persistency;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace E4CTest.be.domain
 {
     [TestClass]
     public class TestRosetta
     {
-        private IRosetta rosetta;
 
         [TestMethod]
         public void TestHappyFlow()
         {
-            rosetta = new Rosetta();
-            string key = "charts.datainputchart.titleform";
-            string expectedValue = "Data input for new chart";
+            IEnumerable<string> allLines = new string[] { "first.subject = MyFirstSubject", "second.subject = MySecondSubject", "third.subject = MyThirdSubject" };
+            var mock = new Mock<ITextFromFileReader>();
+            mock.Setup(p => p.readSeparatedLines(It.IsAny<string>())).Returns(allLines);
+            IRosetta rosetta = new Rosetta(mock.Object);
+            string key = "second.subject";
+            string expectedValue = "MySecondSubject";
             string retrievedValue = rosetta.TextForId(key);
             Assert.AreEqual(expectedValue, retrievedValue);
         }
+
+        [TestMethod]
+        public void TestKeyNotFound()
+        {
+            IEnumerable<string> allLines = new string[] { "first.subject = MyFirstSubject", "second.subject = MySecondSubject", "third.subject = MyThirdSubject" };
+            var mock = new Mock<ITextFromFileReader>();
+            mock.Setup(p => p.readSeparatedLines(It.IsAny<string>())).Returns(allLines);
+            IRosetta rosetta = new Rosetta(mock.Object);
+            string key = "wrong.key";
+            string expectedValue = "-NOT FOUND-";
+            string retrievedValue = rosetta.TextForId(key);
+            Assert.AreEqual(expectedValue, retrievedValue);
+        }
+
+        [TestMethod]
+        public void TestWithEmptyLines()
+        {
+            IEnumerable<string> allLines = new string[] { "first.subject = MyFirstSubject", "", "third.subject = MyThirdSubject" };
+            var mock = new Mock<ITextFromFileReader>();
+            mock.Setup(p => p.readSeparatedLines(It.IsAny<string>())).Returns(allLines);
+            IRosetta rosetta = new Rosetta(mock.Object);
+            string key = "third.subject";
+            string expectedValue = "MyThirdSubject";
+            string retrievedValue = rosetta.TextForId(key);
+            Assert.AreEqual(expectedValue, retrievedValue);
+        }
+
+        [TestMethod]
+        public void TestWithCommentLines()
+        {
+            IEnumerable<string> allLines = new string[] { "first.subject = MyFirstSubject", "#Comment line", "third.subject = MyThirdSubject" };
+            var mock = new Mock<ITextFromFileReader>();
+            mock.Setup(p => p.readSeparatedLines(It.IsAny<string>())).Returns(allLines);
+            IRosetta rosetta = new Rosetta(mock.Object);
+            string key = "third.subject";
+            string expectedValue = "MyThirdSubject";
+            string retrievedValue = rosetta.TextForId(key);
+            Assert.AreEqual(expectedValue, retrievedValue);
+        }
+
+
     }
 }
