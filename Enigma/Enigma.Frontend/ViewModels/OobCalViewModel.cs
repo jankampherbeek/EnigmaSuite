ce1cdbd63ffd60f6a22298d4ab71aca4409385dd
Jan Kampherbeek <jkampherbeek@gmail.com>
2024-05-19 22:03:12 +0200

    View for Oob Calendar now uses texts from resource bundle.

--- Enigma/Enigma.Frontend/ViewModels/OobCalViewModel.cs
+++ Enigma/Enigma.Frontend/ViewModels/OobCalViewModel.cs
@@ -11,6 +11,7 @@ using Enigma.Domain.Presentables;
 using Enigma.Frontend.Ui.Messaging;
 using Enigma.Frontend.Ui.Models;
 using Enigma.Frontend.Ui.State;
+using Enigma.Frontend.Ui.Support;
 using Enigma.Frontend.Ui.WindowsFlow;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,12 +23,20 @@ public partial class OobCalViewModel: ObservableObject
     private readonly int _windowId = DataVaultCharts.Instance.LastWindowId;
     [ObservableProperty] private string _chartId;
     [ObservableProperty] private string _description;
+    [ObservableProperty] private string _btnClose;
+    [ObservableProperty] private string _btnHelp;
+    [ObservableProperty] private string _header; 
+    [ObservableProperty] private string _dayTxt;
+    [ObservableProperty] private string _monthTxt;
+    [ObservableProperty] private string _yearTxt;
+    [ObservableProperty] private string _typeOfChange;
     [ObservableProperty] private ObservableCollection<PresentableOobEvents> _oobEvents;
     public OobCalViewModel()
     {
         var model = App.ServiceProvider.GetRequiredService<OobCalModel>();
         _description = model.DescriptiveText();
         _oobEvents = new ObservableCollection<PresentableOobEvents>(model.GetOobEvents());
+        DefineTexts();
     }
     
     [RelayCommand]
@@ -40,6 +49,18 @@ public partial class OobCalViewModel: ObservableObject
     private static void Help()
     {
         WeakReferenceMessenger.Default.Send(new HelpMessage(VM_IDENTIFICATION));
+    }
+
+    private void DefineTexts()
+    {
+        Rosetta rosetta = Rosetta.Instance;
+        BtnClose = rosetta.GetText("shr_btn_close");
+        BtnHelp = rosetta.GetText("shr_btn_help");
+        Header = rosetta.GetText("vw_oobcal_title");
+        DayTxt = rosetta.GetText("vw_oobcal_day");
+        MonthTxt = rosetta.GetText("vw_oobcal_month");
+        YearTxt = rosetta.GetText("vw_oobcal_year");
+        TypeOfChange = rosetta.GetText("vw_oobcal_type_of_chance");
     }
 
 }
